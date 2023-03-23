b1576a2240f24154aea602541fa7b933ad95243d
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-03-23 10:47:47 +0900

    Add fields to popup menu

--- Assets/Scripts/SettingsPopup.cs
+++ Assets/Scripts/SettingsPopup.cs
@@ -25,4 +25,14 @@ public class SettingsPopup : MonoBehaviour
     {
         gameObject.SetActive(false);
     }
+
+    public void OnSubmitName(string name)
+    {
+        Debug.Log(name);
+    }
+
+    public void OnSpeedValue(float speed)
+    {
+        Debug.Log($"Speed: {speed}");
+    }
 }
