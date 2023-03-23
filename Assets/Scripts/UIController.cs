685fd22ff86c9d3c457809ffda2912a6929543f9
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-03-23 17:37:38 +0900

    Add event system to RayShooter

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -10,16 +10,31 @@ public class UIController : MonoBehaviour
     [SerializeField] TMP_Text scoreLabel;
     [SerializeField] SettingsPopup settingsPopup;
 
+    private int score;
+
     // Start is called before the first frame update
     void Start()
     {
+        score = 0;
+        scoreLabel.text = score.ToString();
+
         settingsPopup.Close();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        scoreLabel.text = Time.realtimeSinceStartup.ToString();
+        Messenger.AddListener(GameEvent.ENEMY_HIT, OnEnemyHit);
+    }
+
+    void OnDisable()
+    {
+        Messenger.RemoveListener(GameEvent.ENEMY_HIT, OnEnemyHit);
+    }
+
+    public void OnEnemyHit()
+    {
+        score += 1;
+        scoreLabel.text = score.ToString();
     }
 
     public void OnOpenSettings()
